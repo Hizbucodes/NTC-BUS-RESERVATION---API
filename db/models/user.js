eb7caba5e7c34e29058edeee22b1a09ce9522265
Hizbucodes <106305260+Hizbucodes@users.noreply.github.com>
2024-11-28 15:23:11 +0530

    generate user model and migration file

---
+++ db/models/user.js
@@ -0,0 +1,45 @@
+"use strict";
+const { Model, Sequelize } = require("sequelize");
+const sequelize = require("../../config/database");
+module.exports = sequelize.define(
+  "User",
+  {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+    },
+    role: {
+      type: Sequelize.ENUM("commuter", "operator", "admin"),
+    },
+    firstName: {
+      type: Sequelize.STRING,
+    },
+    lastName: {
+      type: Sequelize.STRING,
+    },
+    email: {
+      type: Sequelize.STRING,
+    },
+    password: {
+      type: Sequelize.STRING,
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+    deletedAt: {
+      type: Sequelize.DATE,
+    },
+  },
+  {
+    paranoid: true,
+    freezeTableName: true,
+    modelName: "User",
+  }
+);
