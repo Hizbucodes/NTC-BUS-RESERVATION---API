eda57b6ebc5e4a349acbd795114d089e057e55d8
Hizbucodes <106305260+Hizbucodes@users.noreply.github.com>
2024-11-28 17:52:52 +0530

    add signup method to handle user registration process

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const authRouter = require("./routes/authRoute");
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res, next) => {
   res.status(200).json({
     status: "success",
