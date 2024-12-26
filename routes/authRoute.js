09e4bfbc8096a917f05f114134fae9c75a47b66b
Hizbucodes <106305260+Hizbucodes@users.noreply.github.com>
2024-12-27 02:13:06 +0530

    add endpoint to get all users with operator role

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -5,12 +5,17 @@ import {
   verifyToken,
   authentication,
   deleteUser,
+  getAllUsersWithOperatorRole,
+  restrictTo,
 } from "../controller/authController.js";
 
 const router = Router();
 
 router.route("/signup").post(signup);
 router.route("/signin").post(signin);
+router
+  .route("/operators")
+  .get(authentication, restrictTo("admin"), getAllUsersWithOperatorRole);
 router.route("/deleteMe").delete(authentication, deleteUser);
 
 router.route("/userDetails").get(verifyToken);
